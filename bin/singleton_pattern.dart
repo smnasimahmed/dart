bb696c02fc57a7ab514944a7553e76fb037f8716
S M Nasim Ahmed <nasimfarsid@gmail.com>
2025-03-02 17:12:52 +0600

    started to learning singleton pattern

---
+++ bin/singleton_pattern.dart
@@ -0,0 +1,5 @@
+import 'package:singleton_pattern/singleton_pattern.dart' as singleton_pattern;
+
+void main(List<String> arguments) {
+  print('Hello world: ${singleton_pattern.calculate()}!');
+}
---
+++ test/singleton_pattern_test.dart
@@ -0,0 +1,8 @@
+import 'package:singleton_pattern/singleton_pattern.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
