b5eedb4d9ef76c7c0efdde5f97bdb46e06ff8d33
S M Nasim Ahmed <nasimfarsid@gmail.com>
2025-03-03 16:59:22 +0600

    trying given challanges on dart apprentics chapter 06

---
+++ bin/dart_apprentics_chapter06_challanges.dart
@@ -0,0 +1,5 @@
+import 'package:dart_apprentics_chapter06_challanges/dart_apprentics_chapter06_challanges.dart' as dart_apprentics_chapter06_challanges;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_apprentics_chapter06_challanges.calculate()}!');
+}
---
+++ test/dart_apprentics_chapter06_challanges_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_apprentics_chapter06_challanges/dart_apprentics_chapter06_challanges.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
