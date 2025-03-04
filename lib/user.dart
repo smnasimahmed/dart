7907bcdbe4db34182549a7b37abda3393fabc435
S M Nasim Ahmed <111959129+smnasimahmed@users.noreply.github.com>
2025-03-04 18:33:51 +0600

    some comment added json complete

--- lib/user.dart
+++ lib/user.dart
@@ -4,13 +4,15 @@ class User {
   String toJson() {
     return '{"id":$id,"name":$name}';
   }
-
+  
+// Using factory
   factory User.fromJson(Map<String, Object> json) {
     final userId = json['id'] as int;
     final userName = json['name'] as String;
     return User(id: userId, name: userName);
   }
 
+ // Using Static
   // static User fromJson(Map<String, Object> json) {
   //   final userId = json['id'] as int;
   //   final userName = json['name'] as String;
