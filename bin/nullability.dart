141e98ce7459c90c1b1bcd205dcc126071b08d0d
S M Nasim Ahmed <nasimfarsid@gmail.com>
2025-03-09 18:43:04 +0600

    type promotion

--- bin/nullability.dart
+++ bin/nullability.dart
@@ -1,7 +1,15 @@
 void main() {
   print(isPositive(3));
   print(isPositive(-1));
-  print(isPositive(null));
+  // print(isPositive(null)); //will throw error
+
+  int? age;
+  double? height;
+  String? message;
+
+  print(age);
+  print(height);
+  print(message);
 }
 
 bool isPositive(int andInteger) {
