fa7bacaa9e699e91b8c92cd2bd1dfc1e2df47644
S M Nasim Ahmed <nasimfarsid@gmail.com>
2025-03-15 02:47:47 +0600

    Null-aware cascade operator (?..)

--- bin/nullability.dart
+++ bin/nullability.dart
@@ -6,12 +6,40 @@ void main() {
   int? age;
   double? height;
   String? message;
+  // message = '20';
 
   print(age);
   print(height);
   print(message);
+
+  final text = message ?? 'Error';
+  print(text);
+
+  double? fontSize;
+  print(fontSize); //Null
+  fontSize ??= 20.2; //if Null set default 20.2
+  print(fontSize); //20.2
+
+  // age = -3;
+  print(age?.toDouble());
+
+  // Assertion operator!
+  bool flowerIsBeautiful = isBeautiful('flower')!;
+  print(flowerIsBeautiful);
 }
 
-bool isPositive(int andInteger) {
+bool isPositive(int? andInteger) {
+  if (andInteger == null) {
+    return false;
+  }
   return !andInteger.isNegative;
+}
+
+bool? isBeautiful(String item) {
+  if (item == 'flower') {
+    return true;
+  } else if (item == 'garbage') {
+    return false;
+  }
+  return null;
 }
