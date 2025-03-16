2d2cdcf3886a44dfd98269a222f9730c23e1bfd4
S M Nasim Ahmed <nasimfarsid@gmail.com>
2025-03-17 00:15:21 +0600

    null cascade

--- bin/nullability.dart
+++ bin/nullability.dart
@@ -26,6 +26,15 @@ void main() {
   // Assertion operator!
   bool flowerIsBeautiful = isBeautiful('flower')!;
   print(flowerIsBeautiful);
+
+  // Null aware cascade operation
+  User? user;
+  // ?..name = 'Ray'
+  // ..id = 42;
+
+  // ignore: dead_code
+  String? lengthString = user?.name?.length.toString();
+  print(lengthString);
 }
 
 bool isPositive(int? andInteger) {
@@ -33,4 +42,23 @@ bool isPositive(int? andInteger) {
     return false;
   }
   return !andInteger.isNegative;
+}
+
+bool? isBeautiful(String item) {
+  if (item == 'flower') {
+    return true;
+  } else if (item == 'garbage') {
+    return false;
+  }
+  return null;
+}
+
+class User {
+  String? name;
+  int? id;
+
+  @override
+  String toString() {
+    return "$name and $id";
+  }
 }
