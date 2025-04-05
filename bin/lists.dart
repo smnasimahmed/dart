ba9d2da143385571a1ca73abeba26e23da075582
S M Nasim Ahmed <nasimfarsid@gmail.com>
2025-04-06 03:07:12 +0600

    Runtime List immutable

--- bin/lists.dart
+++ bin/lists.dart
@@ -1,13 +1,23 @@
 void main() {
   List<String> desserts = ['coockies', 'cupcakes', 'donuts', 'pie'];
-  final secondElement = desserts[1];
-  final index = desserts.indexOf('pie');
-  print(secondElement);
-  print(desserts);
-  print(index);
-  desserts.add('brownies');
-  print(desserts);
-  desserts.remove('donuts');
-  print(desserts);
+  // final secondElement = desserts[1];
+  // final index = desserts.indexOf('pie');
+  // print(secondElement);
+  // print(desserts);
+  // print(index);
+  // desserts.add('brownies');
+  // print(desserts);
+  // desserts.remove('donuts');
+  // print(desserts);
+
+  final modifiableList = [
+    DateTime.now(),
+    DateTime.now(),
+  ];
+
+  print(modifiableList);
+  // When you dont know what will be the value
+  final unmodifiableList = List.unmodifiable(modifiableList);
+  print(unmodifiableList);
 }
 // Notrhign to commit 
