61d38f1c35cd89bfbda27b35c1d8cfef3d09b1c0
S M Nasim Ahmed <nasimfarsid@gmail.com>
2025-04-10 23:02:45 +0600

    Sets comepleted

--- bin/sets.dart
+++ bin/sets.dart
@@ -1,1 +1,27 @@
-void main() {}
+import 'package:sets/sets.dart';
+
+void main() {
+  print(anotherSet);
+  print(anotherSet.contains(1));
+  print(anotherSet.contains(99));
+  someSet.add(42);
+  someSet.add(2112);
+  someSet.add(42);
+  print(someSet);
+  someSet.remove(2112);
+  print(someSet);
+  someSet.addAll([42, 1, 2, 3, 4]);
+  print(someSet);
+
+  final intersection = setA.intersection(setB);
+  print(intersection);
+  final union = setA.union(setB);
+  print(union);
+}
+
+// Other operations can be used in set
+// collection if
+// collection for
+// for-in loops
+// forEach loops
+// spread operators
--- lib/sets.dart
+++ lib/sets.dart
@@ -1,3 +1,5 @@
-int calculate() {
-  return 6 * 7;
-}
+final Set<int> someSet = {};
+final anotherSet = {1, 2, 3, 1};
+
+final setA = {8, 2, 3, 1, 4};
+final setB = {1, 6, 5, 4};
