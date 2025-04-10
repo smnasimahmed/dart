b6152156036c9e699531e6d7021ac2e0f8112ef1
S M Nasim Ahmed <nasimfarsid@gmail.com>
2025-04-11 00:09:54 +0600

    Maps init

--- bin/sets_and_map.dart
+++ bin/sets_and_map.dart
@@ -1,26 +1,22 @@
 import 'package:sets/sets_and_map_variable.dart';
 
 void main() {
-  print(anotherSet);
-  print(anotherSet.contains(1));
-  print(anotherSet.contains(99));
-  someSet.add(42);
-  someSet.add(2112);
-  someSet.add(42);
-  print(someSet);
-  someSet.remove(2112);
-  print(someSet);
-  someSet.addAll([42, 1, 2, 3, 4]);
-  print(someSet);
+//   print(anotherSet);
+//   print(anotherSet.contains(1));
+//   print(anotherSet.contains(99));
+//   someSet.add(42);
+//   someSet.add(2112);
+//   someSet.add(42);
+//   print(someSet);
+//   someSet.remove(2112);
+//   print(someSet);
+//   someSet.addAll([42, 1, 2, 3, 4]);
+//   print(someSet);
 
-  final intersection = setA.intersection(setB);
-  print(intersection);
-  final union = setA.union(setB);
-  print(union);
-
-  final Map<String, String> treasureMap = {
-
-}
+//   final intersection = setA.intersection(setB);
+//   print(intersection);
+//   final union = setA.union(setB);
+//   print(union);
 
 // Other operations can be used in set
 // collection if
@@ -28,3 +24,11 @@ void main() {
 // for-in loops
 // forEach loops
 // spread operators
+
+// Maps
+  final Map<String, int> emptyMap = {};
+  print(inventory);
+  print(digitToWord);
+  print(treasureMap); // as you can see here is only one gold map
+  print(treasureMapList); // both gold map can be sown for List
+}
