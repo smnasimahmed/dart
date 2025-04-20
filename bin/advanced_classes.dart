5031e1efe866ecb95c8aaa6b788522c19a2ddee7
S M Nasim Ahmed <nasimfarsid@gmail.com>
2025-04-21 00:29:49 +0600

    advanced_classes init

---
+++ bin/advanced_classes.dart
@@ -0,0 +1,5 @@
+import 'package:advanced_classes/advanced_classes.dart' as advanced_classes;
+
+void main(List<String> arguments) {
+  print('Hello world: ${advanced_classes.calculate()}!');
+}
---
+++ test/advanced_classes_test.dart
@@ -0,0 +1,8 @@
+import 'package:advanced_classes/advanced_classes.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
