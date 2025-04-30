03b215b3b8610b655fe232c910ff7f95f93857c8
S M Nasim Ahmed <nasimfarsid@gmail.com>
2025-05-01 01:59:18 +0600

    asynchronous_programming init

---
+++ bin/asynchronous_programming.dart
@@ -0,0 +1,5 @@
+import 'package:asynchronous_programming/asynchronous_programming.dart' as asynchronous_programming;
+
+void main(List<String> arguments) {
+  print('Hello world: ${asynchronous_programming.calculate()}!');
+}
