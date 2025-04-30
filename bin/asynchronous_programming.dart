2c6d5beed64c352b22261208b0ccd0b15b5272b1
S M Nasim Ahmed <nasimfarsid@gmail.com>
2025-05-01 02:00:05 +0600

    asynchronous_programming init

--- bin/asynchronous_programming.dart
+++ bin/asynchronous_programming.dart
@@ -1,5 +1,1 @@
-import 'package:asynchronous_programming/asynchronous_programming.dart' as asynchronous_programming;
 
-void main(List<String> arguments) {
-  print('Hello world: ${asynchronous_programming.calculate()}!');
-}
--- test/asynchronous_programming_test.dart
+++ test/asynchronous_programming_test.dart
@@ -1,8 +1,1 @@
-import 'package:asynchronous_programming/asynchronous_programming.dart';
-import 'package:test/test.dart';
 
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
