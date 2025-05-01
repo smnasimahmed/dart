082cc899f75595dcaf72199adeebc02e55e04d01
S M Nasim Ahmed <nasimfarsid@gmail.com>
2025-05-01 23:56:04 +0600

    Future

--- bin/asynchronous_programming.dart
+++ bin/asynchronous_programming.dart
@@ -1,1 +1,11 @@
+void main() {
+  print('Before the Future');
 
+  final myFuture = Future<int>.delayed(Duration(seconds: 1), () => 42)
+      .then((value) => print('Value: $value'))
+      .catchError((error) => print('Error: $error'))
+      .whenComplete(() => print('Future is complete'));
+  print('Ater the Future');
+
+  myFuture;
+}
