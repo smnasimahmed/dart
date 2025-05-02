da888744789bed3f2d1c6cc28c388ea2a29e0e1b
S M Nasim Ahmed <nasimfarsid@gmail.com>
2025-05-02 23:56:22 +0600

    Try-catch blocks with async-await

--- bin/asynchronous_programming.dart
+++ bin/asynchronous_programming.dart
@@ -1,11 +1,34 @@
-void main() {
-  print('Before the Future');
+// // Callback
+// void main() {
+//   print('Before the Future');
 
-  final myFuture = Future<int>.delayed(Duration(seconds: 1), () => 42)
-      .then((value) => print('Value: $value'))
-      .catchError((error) => print('Error: $error'))
-      .whenComplete(() => print('Future is complete'));
-  print('Ater the Future');
+//   final myFuture = Future<int>.delayed(Duration(seconds: 1), () => 42)
+//       .then((value) => print('Value: $value'))
+//       .catchError((error) => print('Error: $error'))
+//       .whenComplete(() => print('Future is complete'));
+//   print('Ater the Future');
+// }
 
-  myFuture;
+// Getting the result with async-await
+Future<void> main() async {
+  // print("Before the Future");
+
+  // final value = await Future.delayed(Duration(seconds: 1), () => 42);
+
+  // print('Value: $value');
+  // print('After the future');
+
+  // Try-catch blocks with async-await
+  print("Before the Future");
+
+  try {
+    final value = await Future.delayed(Duration(seconds: 1), () => 42);
+    throw Exception('There was an error');
+    print('Value: $value');
+  } catch (error) {
+    print(error);
+  } finally {
+    print("Future is complete");
+  }
+  print('After the future');
 }
