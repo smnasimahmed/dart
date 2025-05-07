5b02dcffc4d55914c0818bac0fb7aa060a7221ac
S M Nasim Ahmed <nasimfarsid@gmail.com>
2025-05-08 01:53:45 +0600

    Reading from a stream

--- lib/streams.dart
+++ lib/streams.dart
@@ -5,4 +5,19 @@ Future<void> readFile() async {
       'F:/Dart/asynchronous_programming/asynchronous_programming/assets/text.txt');
   final contents = await file.readAsString();
   print(contents);
+
+  // Reading from a stream
+  final lngFile = File(
+      'F:/Dart/asynchronous_programming/asynchronous_programming/assets/text_long.txt');
+  final stream = lngFile.openRead();
+  stream.listen((data) => print(data.length));
+
+  // // Made this folder to write Lorium Epsum 1000 time
+  // // on text_long.txt as book instruction
+  // final lngFile = File(
+  //     'F:/Dart/asynchronous_programming/asynchronous_programming/assets/text_long.txt');
+
+  // for (var i = 0; i < 1000; i++) {
+  //   lngFile.writeAsStringSync('$contents \n', mode: FileMode.append);
+  // }
 }
